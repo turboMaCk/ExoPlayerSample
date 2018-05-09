450026433d47a4c2041e599f5ad042ddc7bd2419
Marek Fajkus <marek.faj@gmail.com>
2018-05-09 22:43:31 +0200

    Make video work with HTTP public stream

--- app/src/main/java/com/user/exokotlin/VideoPlayerActivity.kt
+++ app/src/main/java/com/user/exokotlin/VideoPlayerActivity.kt
@@ -3,67 +3,44 @@ package com.user.exokotlin
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
 import com.google.android.exoplayer2.util.Util
-import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
-import com.google.android.exoplayer2.upstream.*
-import com.google.android.exoplayer2.extractor.ExtractorsFactory
-import com.google.android.exoplayer2.extractor.ts.TsExtractor.MODE_SINGLE_PMT
-import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.LoopingMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.upstream.UdpDataSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.trackselection.TrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelection
-import com.google.android.exoplayer2.upstream.BandwidthMeter
-import com.user.exokotlin.R
-
 
 class VideoPlayerActivity : AppCompatActivity() {
-
     private lateinit var player: SimpleExoPlayer
     private var shouldAutoPlay: Boolean = false
-    private val uriString = "udp://@239.255.0.1:5004" // multicast streaming & IP & port
+    private val uri = Uri.parse("http://54.255.155.24:1935//Live/_definst_/amlst:sweetbcha1novD235L240P/playlist.m3u8")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
         shouldAutoPlay = true
-
     }
 
-
-    private fun initPlayer(){
+    private fun initPlayer() {
         val simpleExoPlayerView = findViewById<SimpleExoPlayerView>(R.id.player_view)
         val bandwidthMeter = DefaultBandwidthMeter()
-        val extractorsFactory = DefaultExtractorsFactory()
         val trackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
         val trackSelector = DefaultTrackSelector(trackSelectionFactory)
-        val udpDataSource = DefaultDataSourceFactory(this, Util.getUserAgent(this, "MainActivity"), bandwidthMeter)
-        val mediaSource = ExtractorMediaSource(Uri.parse(uriString), udpDataSource, extractorsFactory, null, null)
+        val dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), bandwidthMeter)
+        val extractorsFactory = DefaultExtractorsFactory()
+        val videoSource = HlsMediaSource(uri, dataSourceFactory, 1, null, null)
+        val loopingSource =  LoopingMediaSource(videoSource)
 
         simpleExoPlayerView.requestFocus()
-
-
         player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
-
-        simpleExoPlayerView.player = player
+        player.prepare(loopingSource)
         player.playWhenReady = shouldAutoPlay;
-        player.prepare(mediaSource)
-
-
+        simpleExoPlayerView.player = player
     }
-
-
-
-
-
 
     private fun releasePlayer() {
         player.release()
@@ -73,7 +50,6 @@ class VideoPlayerActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         initPlayer()
-
     }
 
     override fun onResume() {
@@ -91,6 +67,5 @@ class VideoPlayerActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         releasePlayer()
-
     }
 }
